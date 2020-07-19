2ea17dda68fb869b0df99efdfd9f26bf1df6bcf1
siddharth <ssiddharth014@gmail.com>
2020-07-20 04:33:16 +0530

    second

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
 });
 app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../build/index.html`));
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
 });
 
-app.listen(config.PORT, () => {
+app.listen( process.env.PORT || 5000, () => {
   console.log('Server started at http://localhost:5000');
 });
 
